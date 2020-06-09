ece3f9fd87a03122164357b710f83b87409ddeb5
Lionel Tolosa <lioneltolosaarmas@gmail.com>
2020-06-09 23:31:03 +0200

    Create Routing Module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DashboardComponent } from './views/dashboard/dashboard.component';
+import { UsersComponent } from './views/users/users.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'dashboard', component: DashboardComponent },
+    { path: 'users', component: UsersComponent },
+    { path: '', redirectTo: 'dashboard', pathMatch: 'full' }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
