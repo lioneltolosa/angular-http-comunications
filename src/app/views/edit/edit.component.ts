9356baa62cd150ad4d7afdef912a5da1f51c2f46
Lionel Tolosa <lioneltolosaarmas@gmail.com>
2020-06-10 11:17:33 +0200

    Create edit component

---
+++ src/app/views/edit/edit.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { User } from '../../models/user';
+import { DataService } from '../../services/data.service';
+import { pluck } from 'rxjs/operators';
+
+@Component({
+    selector: 'app-edit',
+    templateUrl: './edit.component.html',
+    styleUrls: ['./edit.component.scss']
+})
+export class EditComponent implements OnInit {
+
+    selectedUser: User;
+
+    constructor(private route: ActivatedRoute,
+                private dataService: DataService,
+            ) { }
+
+    ngOnInit(): void {
+        //let id = +this.route.snapshot.params.get('id');
+        let id = +this.route.snapshot.params.id
+        
+        this.dataService.getUserById(id)
+            .pipe(
+                pluck('data')
+            )
+            .subscribe((user: User) => {
+                this.selectedUser = user;
+                console.log('User', user);
+            })
+    }
+
+}
