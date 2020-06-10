0da61b71b874f5a1489339f747177cdd00daf19e
Lionel Tolosa <lioneltolosaarmas@gmail.com>
2020-06-10 17:18:25 +0200

    Create User

--- src/app/views/users/users.component.ts
+++ src/app/views/users/users.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
+import { User } from '../../models/user';
 
 @Component({
-  selector: 'app-users',
-  templateUrl: './users.component.html',
-  styleUrls: ['./users.component.scss']
+    selector: 'app-users',
+    templateUrl: './users.component.html',
+    styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+    constructor(private dataService: DataService) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
+
+    saveUser(newUser: User) {
+        this.dataService.createUser(newUser)
+            .subscribe(
+                (data: User) => console.log(data),
+                (err: any) => console.log('Err', err),
+            )
+    }
 
 }
