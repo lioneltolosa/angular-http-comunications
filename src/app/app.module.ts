67d3402ecc67ccdffbb5dd2b35ce89be559b5d02
Lionel Tolosa <lioneltolosaarmas@gmail.com>
2020-06-11 22:37:56 +0200

    Create 2 Unit Test for http Request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,23 +9,25 @@ import { DashboardComponent } from './views/dashboard/dashboard.component';
 import { UsersComponent } from './views/users/users.component';
 import { EditComponent } from './views/edit/edit.component';
 import { AddHeadersInterceptor } from './services/add-headers.interceptor';
+import { CacheInterceptor } from './services/cache.interceptor';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    DashboardComponent,
-    UsersComponent,
-    EditComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    FormsModule
-  ],
-  providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: AddHeadersInterceptor, multi: true },
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        DashboardComponent,
+        UsersComponent,
+        EditComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        FormsModule
+    ],
+    providers: [
+        { provide: HTTP_INTERCEPTORS, useClass: AddHeadersInterceptor, multi: true },
+        { provide: HTTP_INTERCEPTORS, useClass: CacheInterceptor, multi: true },
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/services/data.service.spec.ts
+++ src/app/services/data.service.spec.ts
@@ -1,16 +1,63 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController, TestRequest } from '@angular/common/http/testing';
 
 import { DataService } from './data.service';
+import { User } from '../models/user';
+import { UserError } from '../models/userError';
 
 describe('DataService', () => {
-  let service: DataService;
+    let dataService: DataService;
+    let httpTestingController: HttpTestingController;
 
-  beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(DataService);
-  });
+    let testUsers: User[] = [
+        { id: 1, first_name: 'George', last_name: 'Bluth', email: 'george.bluth@reqres.in', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/calebogden/128.jpg' },
+        { id: 2, first_name: 'Janet', last_name: 'Weaver', email: 'janet.weaver@reqres.in', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/josephstein/128.jpg' },
+        { id: 3, first_name: 'Emma', last_name: 'Wong', email: 'emma.wong@reqres.in', avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/olegpogodaev/128.jpg' }
+    ];
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
-  });
+    beforeEach(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule],
+            providers: [DataService]
+        });
+        dataService = TestBed.get(DataService);
+        httpTestingController = TestBed.get(HttpTestingController);
+    });
+
+    afterEach(() => {
+        httpTestingController.verify();
+    });
+
+    it('should GET all Users', () => {
+        dataService.getAllUsers()
+            .subscribe((data: User[]) => {
+                expect(data.length).toBe(3)
+            })
+
+        let usersRequest: TestRequest = httpTestingController.expectOne('https://reqres.in/api/users');
+
+        expect(usersRequest.request.method).toEqual('GET');
+
+        usersRequest.flush(testUsers);
+    });
+
+    it('should return a BookTrackerError', () => {
+
+        dataService.getAllUsers()
+            .subscribe(
+                (data: User[]) => fail('this should have been an error'),
+                (err: UserError) => {
+                    expect(err.errorNumber).toEqual(100);
+                    expect(err.friendlyMessage).toEqual('An error occurred retrieving data.');
+                }
+            );
+
+        let usersRequest: TestRequest = httpTestingController.expectOne('https://reqres.in/api/users');
+
+        usersRequest.flush('error', {
+            status: 500,
+            statusText: 'Server Error'
+        });
+
+    });
 });
