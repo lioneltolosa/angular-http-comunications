ff9866384a840d9f9a45946c0cc882838d2c07ba
Lionel Tolosa <lioneltolosaarmas@gmail.com>
2020-06-25 14:42:11 +0200

    Add Routes Parameters and use ActivatedRoute with snapshot

--- src/app/products/components/product-details/product-details.component.ts
+++ src/app/products/components/product-details/product-details.component.ts
@@ -12,18 +12,30 @@ export class ProductDetailsComponent implements OnInit {
     
     pageTitle = 'Product Detail';
     product: ProductModel;
+    errorMessage: string;
 
     constructor(private productServive: ProductsService,
                 private route: ActivatedRoute) { }
 
     ngOnInit(): void {
-        let id = +this.route.snapshot.params.id;
+        /* let id = +this.route.snapshot.params.id; */
+        const id = +this.route.snapshot.paramMap.get('id'); // el mas es para convertirlo de string a numeric
 
         this.productServive.getProductsById(id)
-            .subscribe(productDetails => {
-                console.log('productDetails', productDetails);
-                this.product = productDetails
-            })
+        .subscribe({
+            next: product => this.onProductRetrieved(product),
+            error: err => this.errorMessage = err
+          });
+    }
+
+    onProductRetrieved(product: ProductModel): void {
+        this.product = product;
+
+        if(this.product) {
+            this.pageTitle = `Product Detail: ${this.product.productName}`;
+        } else {
+            this.pageTitle = 'No product found';
+        }
     }
 
 }
